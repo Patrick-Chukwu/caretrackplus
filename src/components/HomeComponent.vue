ef8af1c32fd89bae4097d30b4afebb69a5a7db6b
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-05-02 04:50:44 +0100

    Update file Build login page and route it

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,6 +2,8 @@
   <div class="home column">
     <h2 class="big_text">Welcome to CareTrack+</h2>
   <router-link to="/onboard" class="button">Onboard page</router-link>
+  <router-link to="/login" class="button">Login page</router-link>
+
 
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import AboutComponent from './components/AboutComponent'
 import HomeComponent from './components/HomeComponent'
 import OnboardComponent from './components/OnboardComponent'
+import LoginComponent from './components/LoginComponent'
 
 import App from './App.vue'
 
@@ -11,7 +12,9 @@ const router = createRouter({
     routes:[
         { path: '/about', component: AboutComponent},
         { path: '/', component: HomeComponent},
-        { path: '/onboard', component: OnboardComponent}
+        { path: '/onboard', component: OnboardComponent},
+        { path: '/login', component: LoginComponent}
+
 
     ]
 });
