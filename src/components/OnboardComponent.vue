836f3118c7ff02e82f48dae4bc082c8c4427e408
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-05-24 19:21:06 +0100

    Update file

--- src/components/OnboardComponent.vue
+++ src/components/OnboardComponent.vue
@@ -3,16 +3,16 @@
         <router-link to="/" class="logo_box">
           <img class="logo" src="../../public/logofull.svg" alt="CareTrack+ logo">
         </router-link>
-        <!-- <div class="nav_menus row">
+        <div class="nav_menus row">
   <router-link to="/onboard" class="nav_menu">Solution</router-link>
   <router-link to="/login" class="nav_menu">Pricing</router-link>
   <router-link to="/onboard" class="nav_menu">Resources</router-link>
   <router-link to="/contact" class="nav_menu">Contact</router-link>
-        </div> -->
+        </div>
         <div class="nav_buttons row">
           <router-link to="/login" class="button">Login</router-link>
 
-          <router-link to="/onboard" class="button">Sign up</router-link>
+          <!-- <router-link to="/onboard" class="button">Sign up</router-link> -->
 
         </div>
       </nav>
@@ -33,21 +33,21 @@
     <label class="label" for="number">Phone Number</label>
     <input type="Number"  id="number" >
   </div>
-
+<!-- 
     <div class="row form_box">
     <label class="label" for="auname">Admin User Name</label>
     <input type="text"  id="auname" >
-  </div>
+  </div> -->
 
-    <div class="row form_box">
+    <!-- <div class="row form_box">
     <label class="label" for="password">Password</label>
     <input type="password"  id="password" >
-  </div>
+  </div> -->
 
-    <div class="row form_box">
+    <!-- <div class="row form_box">
     <label class="label" for="confirmpassword">Confirm Password</label>
     <input type="password"  id="confirmpassword" >
-  </div>
+  </div> -->
 
     <div class="row form_box">
     <label class="label last_label" for="hospitallogo">Upload Hospital Logo</label>
@@ -62,9 +62,9 @@
         </label>
     <input class="default_upload" type="file"  id="hospitallogo" placeholder="type">
   </div>
-
+  <button type="submit" class="button onboard_button" onclick="alert('Welcome onboard!')">Onboard</button>
   </form>
-  <button type="submit" class="button" onclick="alert('Welcome onboard!')">Onboard</button>
+
   <div class="row powered_by">
       <h3 class="footer_text">Powered by:</h3>
     <router-link to="/" class="logo_box"><h3 style="color: var(--primary-color);">CareTrack <span style="color: var(--secondary-color);">+</span></h3></router-link>
